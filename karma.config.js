8be3351c804b1ed5760f15c2f1bc50b8a4c70c5c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-17 09:42:29 -0500

    configure testing environment

---
+++ karma.config.js
@@ -0,0 +1,41 @@
+const webpack = require('./webpack.config')({mode: 'test'});
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      {
+        pattern: './karma.shim.js',
+        watched: false
+      }
+    ],
+
+    preprocessors: {
+      './karma.shim.js': [
+        'webpack',
+        'sourcemap'
+      ]
+    },
+
+    webpack,
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true
+  });
+};
