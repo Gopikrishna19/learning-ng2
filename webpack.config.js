6f29a9971909c15cbdaf6f5652b4146e5993c2a8
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-17 09:42:15 -0500

    configure bundling for different environments

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+
+const staticFilesPattern = /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/;
+const staticLoader = {
+  file: {
+    test: staticFilesPattern,
+    loader: 'file-loader?name=assets/[name].[hash].[ext]'
+  },
+  test: {
+    test: staticFilesPattern,
+    loader: 'null-loader'
+  }
+};
+
+const config = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract([
+          'css-loader',
+          'sass-loader'
+        ])
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+  plugins: [
+    new Webpack.optimize.CommonsChunkPlugin({name: ['app', 'vendor', 'polyfills']}),
+    new ExtractTextPlugin('[name].[hash].css'),
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+};
+
+module.exports = (env = {}) => {
+
+  switch (env.mode) {
+
+    case 'prod':
+      return merge(config, {
+        module: {loaders: [staticLoader.file]},
+        plugins: [
+          new Webpack.NoErrorsPlugin(),
+          new Webpack.optimize.DedupePlugin(),
+          new Webpack.optimize.UglifyJsPlugin({mangle: {keep_fnames: true}}),
+          new Webpack.DefinePlugin({env: JSON.stringify(env)})
+        ]
+      });
+
+    case 'test':
+      return merge(config, {
+        module: {loaders: [staticLoader.test]},
+      });
+
+    default:
+      return merge(config, {
+        devServer: {
+          contentBase: './',
+          hot: true
+        },
+        entry: {hot: 'webpack-dev-server/client?http://localhost:8080/'},
+        module: {loaders: [staticLoader.file]},
+        plugins: [new Webpack.HotModuleReplacementPlugin()]
+      });
+
+  }
+
+};
