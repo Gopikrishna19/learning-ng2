5d7958f41f3163f04ad0a6db8c33704716b0d31d
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-06-17 11:49:17 -0500

    initial component

---
+++ src/app/start/app.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'main-app',
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import "./index.scss";
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app';
+import './index.scss';
+
+declare let env: any;
+
+if (env.mode === 'prod') {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
